ed9c203cd2c4261f20802247fb67e4fb1cfd91df
Nikhil Pawar <nikhilpawar151@gmail.com>
2023-04-21 16:12:32 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,45 @@
+
+# Daily Stock Update (Web Scrape)
+
+In this project we have will get top 10 stocks from Nifty 500 and send email. Automatically execute the code if we set
+
+
+
+
+## Create App Password (Gmail Account)
+
+To create an app password for your Gmail account, please follow these steps:
+
+- Log in to your Gmail account.
+- Click on your profile picture in the top right corner and select "Google Account".
+- Click on "Security" on the left-hand side menu.
+- Scroll down to "Signing in to Google" and click on "App passwords".
+- Select the app and device you want to generate the app password for.
+- Follow the instructions on the screen to generate the app password.
+
+Use the app password instead of your regular password when prompted by the app or device.
+
+
+## Create Scheduler in Your system (For Auto Alert)
+You can execute code manually at any time. But if you want to execute the python code automatically at certain time in a day. If you don't want to schdule task then steps not required. 
+
+To execute this code daily at 4 PM, you can use a task scheduling system provided by your operating system. Here's an example of how to schedule this script to run daily at 4 PM using the built-in Windows Task Scheduler on a Windows 10 computer:
+
+- Open the Task Scheduler by pressing the Windows key + R, typing "taskschd.msc" in the Run dialog box, and pressing Enter.
+- Click on "Create Task" in the "Actions" pane on the right-hand side.
+- In the "General" tab, give your task a name and a description (optional).
+- In the "Triggers" tab, click "New" to create a new trigger. Set the trigger to run "Daily" at 4:00 PM, and choose the start date and time.
+- In the "Actions" tab, click "New" to create a new action. Set the action to "Start a program", and browse to the location of your Python executable (e.g., C:\Python39\python.exe).
+- In the "Add arguments" field, enter the full path to your Python script that you want to execute at 4:00 PM daily. For example: "C:\path\to\your\script.py".
+- In the "Conditions" tab, you can set any additional conditions for the task to run, such as only when the computer is plugged in, or only if a certain amount of idle time has passed.
+- Click "OK" to create the task.
+- Your task should now appear in the list of tasks in the Task Scheduler. You can right-click on it and choose "Run" to test it out, or wait until 4:00 PM to see if it runs automatically.
+
+
+
+## Tech Stack
+
+**Libraries:** BeautifulSoup, requests, datetime, SMTP_SSL, EmailMessage, pandas, build_table
+
+**Note:- The code used in this project is only for the educational purpose.**
+
---
+++ dailyStockUpdate.py
@@ -0,0 +1,92 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+
+from smtplib import SMTP_SSL
+from email.message import EmailMessage
+import pandas as pd
+from pretty_html_table import build_table
+
+URL = "https://www.hdfcsec.com/market/equity/top-gainer-nse?indicesCode=22115"
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+list = soup.find("div", {"id": "marketTodaycompanyList"})
+stocks = list.find_all("div", class_="companyList")
+print(len(stocks))
+data = []
+count = 0
+header = ["Company", "LTP", "Gain", "Gain %", "D Low", "D High", "Volume"]
+print(header)
+for s in stocks :
+    company = s.find("span", class_="cd-heading").text
+    price = s.find("div", class_="companyDetail").find_all("div", class_="col-md-4")
+    ltp = price[0].find("span", class_="cd-val").text
+    gain = price[1].find("span", class_="cd-val").text
+    gain_per = price[2].find("span", class_="cd-val").text
+    s_range = s.find("div", class_="sliderDiv").find("div", class_="row").find_all("div", class_="col-md-4")
+    day_low = s_range[0].find("span", class_="cd-val").text
+    day_high = s_range[2].find("span", class_="cd-val").text
+    vol = s.find("div", class_="volDiv").find("div", class_="row").find("div", class_="col-md-5").find("span", class_="value").text
+    
+    row = [company, ltp, gain, gain_per, day_low, day_high, vol]
+    #print(row)
+    data.append(row)
+    count += 1
+    if count >= 10 :
+        break
+
+#print(data)
+df = pd.DataFrame(data, columns=["Company", "LTP", "Gain", "Gain %", "Day Low", "Day High", "Volume"])
+print(df)
+
+name = "Nilesh"
+body = """
+<html>
+<head>
+</head>
+<body>
+Hi {1},
+<br><br>
+Please find below the Market Update of 10 stocks from Nifty 500. 
+        {0}
+<br/><br/>
+--<br/>
+Regards,<br/>
+Nikhil Pawar,<br/>
+nikhilpawar151@gmail.com
+</body>
+</html>
+""".format(
+    build_table(
+        df,
+        "blue_light",
+        width="auto",
+        font_family="Open Sans",
+        font_size="13px",
+        text_align="justify",
+    ),
+    name,
+)
+
+def send_mail(SENDER_EMAIL, RECEIVER_EMAIL, MAIL_PASSWORD, subject, body):
+    msg = EmailMessage()
+    msg['From'] = SENDER_EMAIL
+    msg['To'] = RECEIVER_EMAIL
+    msg['Subject'] = subject
+    msg.add_alternative(body, subtype="html")
+
+    with SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(SENDER_EMAIL, MAIL_PASSWORD)
+        smtp.send_message(msg)
+        smtp.close()
+    print("Mail Sent Successfully")
+
+today = datetime.date.today()
+subject = f'Nifty 500 | Top 10 stocks | {today}'
+SENDER_EMAIL = "<Add your email ID>"
+MAIL_PASSWORD = "<Add your App Code from Gmail account>"
+RECEIVER_EMAIL = "<Add receiver's email ID>"
+
+send_mail(SENDER_EMAIL, RECEIVER_EMAIL, MAIL_PASSWORD, subject, body)
